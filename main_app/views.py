58a569a3abf8a51b89bb838c5abb66c0594259b3
Chanjong Pak <cjpak@Chanjongs-MacBook-Air.local>
2022-11-13 13:48:00 -0500

    working authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signup(request):
+    error_messsage = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_messsage = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_messsage}
+    return render(request, 'registration/signup.html', context)
+
 def home(request):
     return render(request, 'home.html')
