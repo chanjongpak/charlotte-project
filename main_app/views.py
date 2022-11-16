4acde9c79da4ce022ca60c14ad2f8f1154cabf74
Chanjong Pak <chanjong.pak@gmail.com>
2022-11-16 16:21:25 -0500

    s3 bucket working

--- main_app/views.py
+++ main_app/views.py
@@ -47,3 +47,17 @@ class EventUpdate(UpdateView):
     model = Event
     fields = '__all__'
     success_url = '/events/saved/'
+
+def add_photo(request, event_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url, event_id=event_id)
+            photo.save()
+        except:
+            print('Error occurred uploading image to S3')
+    return redirect('detail', event_id=event_id)
