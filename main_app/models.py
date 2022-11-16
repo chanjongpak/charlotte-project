5f2e3bad7029d3f569756bedb07d88d523665488
donaldyang <Donald_Yang17@hotmail.com>
2022-11-16 17:33:05 -0500

    styling for border and landing page (landing page to show all events made by all users)

--- main_app/models.py
+++ main_app/models.py
@@ -20,8 +20,7 @@ class Event(models.Model):
     location = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     category = models.CharField(max_length=1, choices=CATEGORIES, default=CATEGORIES[0][0])
-
-
+    
     def __str__(self):
         return self.name
 
