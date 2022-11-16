c34b9e7c8843dfda2e0fddec4b02d99cf4c40dbb
Nicholas Allicock <nicholasallicock@outlook.com>
2022-11-16 16:39:58 -0500

    added comments class and foreign key in models

--- main_app/models.py
+++ main_app/models.py
@@ -14,12 +14,16 @@ CATEGORIES = (
     ('M', 'Marketing'),
 )
 # Create your models here.
+class Comment(models.Model):
+    text = models.CharField(max_length = 50)
+
 class Event(models.Model):
     name = models.CharField(max_length=50)
     date = models.DateField('Event Date')
     location = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     category = models.CharField(max_length=1, choices=CATEGORIES, default=CATEGORIES[0][0])
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
 
 
     def __str__(self):
