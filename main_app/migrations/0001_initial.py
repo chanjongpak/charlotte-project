80c6b50b4b416556f67adca68afd0278b0d6fdfe
donaldyang <Donald_Yang17@hotmail.com>
2022-11-16 22:02:07 -0500

    deleted and re created migrations and -310 files

--- main_app/migrations/0001_initial.py
+++ main_app/migrations/0001_initial.py
@@ -13,13 +13,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Comment',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('text', models.CharField(max_length=50)),
-            ],
-        ),
-        migrations.CreateModel(
             name='Event',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -28,7 +21,7 @@ class Migration(migrations.Migration):
                 ('location', models.CharField(max_length=50)),
                 ('address', models.CharField(max_length=50)),
                 ('category', models.CharField(choices=[('B', 'Business Meeting'), ('U', 'UXDI'), ('S', 'Software Engineer'), ('D', 'Designer'), ('F', 'Finance'), ('R', 'Real Estate'), ('I', 'Self-Improvement'), ('M', 'Marketing')], default='B', max_length=1)),
-                ('comments', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='main_app.comment')),
+                ('user', models.CharField(max_length=1)),
             ],
         ),
         migrations.CreateModel(
